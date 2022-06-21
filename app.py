6e007110f75099dc57331b42238ad6b7f8d34542
Korhan Mutludoğan <korhanmd@gmail.com>
2022-06-21 10:06:32 +0300

    Store toggle status in an attribute

--- app.py
+++ app.py
@@ -7,12 +7,16 @@ class MainWindow(QMainWindow):
 	def __init__(self):
 		super().__init__()
 
+		self.button_is_checked = True
+
 		self.setWindowTitle("My App")
 
 		button = QPushButton("Press Me!")
 		button.setCheckable(True)
 		button.clicked.connect(self.the_button_was_clicked)
 		button.clicked.connect(self.the_button_was_toggled)
+
+		button.setChecked(self.button_is_checked)
 
 		self.setFixedSize(QSize(400, 300))
 
