1a9a0601c4da86f376a65d03dee35874fd4278c2
Korhan Mutludoğan <korhanmd@gmail.com>
2022-06-21 12:33:18 +0300

    Remove old signals and slots from app

--- app.py
+++ app.py
@@ -7,33 +7,11 @@ class MainWindow(QMainWindow):
 	def __init__(self):
 		super().__init__()
 
-		self.button_is_checked = True
-
 		self.setWindowTitle("My App")
 
 		self.button = QPushButton("Press Me!")
-		self.button.setCheckable(True)
-		
-		self.button.clicked.connect(self.the_button_was_clicked)
-		self.button.clicked.connect(self.the_button_was_toggled)
-		self.button.released.connect(self.the_button_was_released)
-
-		self.button.setChecked(self.button_is_checked)
 
 		self.setCentralWidget(self.button)
-
-	def the_button_was_clicked(self):
-		print("Clicked")
-
-	def the_button_was_toggled(self, checked):
-		self.button_is_checked = checked
-
-		print(self.button_is_checked)
-
-	def the_button_was_released(self):
-		self.button_is_checked = self.button.isChecked()
-
-		print(self.button_is_checked)
 
 app = QApplication(sys.argv)
 
