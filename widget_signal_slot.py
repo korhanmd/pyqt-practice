fc28f8048b36bd2469a3b0500bf64350ef5bf391
Korhan Mutludoğan <korhanmd@gmail.com>
2022-06-23 11:42:16 +0300

    Create new app

---
+++ widget_signal_slot.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
+
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
