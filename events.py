7a8b5bca75ed2f1300678adfe2f2e1c53737bc9c
Korhan Mutludoğan <korhanmd@gmail.com>
2022-06-27 09:20:33 +0300

    Create an app for adding events

---
+++ events.py
@@ -0,0 +1,18 @@
+import sys
+
+#from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QTextEdit
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.label = QLabel("Click in this window")
+        self.setCentralWidget(self.label)
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
