89523e4657c549a16da23a0b843e7b9a0eaee0e4
Korhan Mutludoğan <korhanmd@gmail.com>
2022-06-28 11:33:38 +0300

    Add color class to visualize the layouts

--- layout.py
+++ layout.py
@@ -2,13 +2,23 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt5.QtGui import QPalette, QColor
 
+class Color(QWidget):
+
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
+
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("My App")
-        
+
 
 app = QApplication(sys.argv)
 
