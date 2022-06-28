98ccbd30512f33ea4c77d7034a0516470d2c4787
Korhan Mutludoğan <korhanmd@gmail.com>
2022-06-28 15:00:40 +0300

    Add option for different layout types

--- layout.py
+++ layout.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QGridLayout
 from PyQt5.QtGui import QPalette, QColor
 
 class Color(QWidget):
@@ -13,41 +13,57 @@ class Color(QWidget):
         self.setPalette(palette)
 
 class MainWindow(QMainWindow):
+    """ Takes layout mode as parameter
+    0 for grid layout
+    1 for horizontal and vertical layouts
+    """
 
-    def __init__(self):
+    def __init__(self, layout_mode):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("My App")
 
-        layout1 = QHBoxLayout()
-        layout2 = QVBoxLayout()
-        layout3 = QVBoxLayout()
+        if (layout_mode):
+            layout1 = QHBoxLayout()
+            layout2 = QVBoxLayout()
+            layout3 = QVBoxLayout()
 
-        layout1.setContentsMargins(0,0,0,0)
-        layout1.setSpacing(20)
+            layout1.setContentsMargins(0,0,0,0)
+            layout1.setSpacing(20)
 
-        layout2.addWidget(Color('red'))
-        layout2.addWidget(Color('yellow'))
-        layout2.addWidget(Color('purple'))
+            layout2.addWidget(Color('red'))
+            layout2.addWidget(Color('yellow'))
+            layout2.addWidget(Color('purple'))
 
-        layout1.addLayout(layout2)
+            layout1.addLayout(layout2)
 
-        layout1.addWidget(Color('green'))
+            layout1.addWidget(Color('green'))
 
-        layout3.addWidget(Color('red'))
-        layout3.addWidget(Color('purple'))
+            layout3.addWidget(Color('red'))
+            layout3.addWidget(Color('purple'))
 
-        layout1.addLayout(layout3)
+            layout1.addLayout(layout3)
+        else:
+            layout = QGridLayout()
+
+            layout.addWidget(Color('red'), 0, 0)
+            layout.addWidget(Color('green'), 1, 0)
+            layout.addWidget(Color('blue'), 1, 1)
+            layout.addWidget(Color('purple'), 2, 1)
 
         widget = QWidget()
-        widget.setLayout(layout1)
+
+        if (layout_mode):
+            widget.setLayout(layout1)
+        else:
+            widget.setLayout(layout)
 
         self.setCentralWidget(widget)
 
 
 app = QApplication(sys.argv)
 
-window = MainWindow()
+window = MainWindow(0)
 window.show()
 
 app.exec()
