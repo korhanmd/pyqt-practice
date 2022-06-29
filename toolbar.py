ae259beb0c8662e4116f2b2be899baa608ecd642
Korhan Mutludoğan <korhanmd@gmail.com>
2022-06-29 11:36:56 +0300

    Add action to the app

--- toolbar.py
+++ toolbar.py
@@ -21,6 +21,11 @@ class MainWindow(QMainWindow):
         toolbar = QToolBar("My main toolbar")
         self.addToolBar(toolbar)
 
+        button_action = QAction("Your button", self)
+        button_action.setStatusTip("This is your button")
+        button_action.triggered.connect(self.onMyToolBarButtonClick)
+        
+
     def onMyToolBarButtonClick(self, s):
         print("click", s)
 
