6b60b519e471a59417402035b1072f61adccbf45
Korhan Mutludoğan <korhanmd@gmail.com>
2022-06-30 08:59:08 +0300

    Make the action toggleable

--- toolbar.py
+++ toolbar.py
@@ -24,6 +24,7 @@ class MainWindow(QMainWindow):
         button_action = QAction("Your button", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.onMyToolBarButtonClick)
+        button_action.setCheckable(True)
         toolbar.addAction(button_action)
 
         self.setStatusBar(QStatusBar(self))
