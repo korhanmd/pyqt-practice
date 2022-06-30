1d8392ba676d9961f02f9a9a0de8f35cafb35343
Korhan Mutludoğan <korhanmd@gmail.com>
2022-06-30 15:05:52 +0300

    Add seperator and the other action to the menu

--- toolbar.py
+++ toolbar.py
@@ -47,6 +47,8 @@ class MainWindow(QMainWindow):
 
         file_menu = menu.addMenu("&File")
         file_menu.addAction(button_action)
+        file_menu.addSeparator()
+        file_menu.addAction(button_action2)
 
     def onMyToolBarButtonClick(self, s):
         print("click", s)
