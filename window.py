acc5468125ff0ae7c952915fbf39f0b0d47bbe89
Korhan Mutludoğan <korhanmd@gmail.com>
2022-07-01 16:09:53 +0300

    Transmit extra data with signal to toggle all windows with single method

--- window.py
+++ window.py
@@ -26,30 +26,27 @@ class MainWindow(QMainWindow):
 
         l = QVBoxLayout()
         button1 = QPushButton("Push for Window 1")
-        button1.clicked.connect(self.toggle_window1)
+        button1.clicked.connect(
+            lambda checked: self.toggle_window(self.window1)
+        )
         l.addWidget(button1)
 
         button2 = QPushButton("Push for Window 2")
-        button2.clicked.connect(self.toggle_window2)
+        button2.clicked.connect(
+            lambda checked: self.toggle_window(self.window2)
+        )
         l.addWidget(button2)
 
         w = QWidget()
         w.setLayout(l)
         self.setCentralWidget(w)
 
-    def toggle_window1(self, checked):
-        if self.window1.isVisible():
-            self.window1.hide()
+    def toggle_window(self, window):
+        if window.isVisible():
+            window.hide()
 
         else:
-            self.window1.show()
-
-    def toggle_window2(self, checked):
-        if self.window2.isVisible():
-            self.window2.hide()
-
-        else:
-            self.window2.show()
+            window.show()
 
 
 app = QApplication(sys.argv)
