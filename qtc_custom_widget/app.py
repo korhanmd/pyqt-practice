810c4d65526c8d3b0b4bbf951e96a550789b9455
Korhan Mutludoğan <korhanmd@gmail.com>
2022-07-27 12:30:28 +0300

    Make a simple plot on pyqtgraph

--- qtc_custom_widget/app.py
+++ qtc_custom_widget/app.py
@@ -11,6 +11,11 @@ class MainWindow(QtWidgets.QMainWindow):
         #Load the UI Page
         uic.loadUi('mainwindow.ui', self)
 
+        self.plot([1,2,3,4,5,6,7,8,9,10], [30,32,34,32,33,31,29,32,35,45])
+
+    def plot(self, hour, temperature):
+        self.graphWidget.plot(hour, temperature)
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
